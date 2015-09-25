7cd2baf4157cd4cf53905d78d441b961f9bde511
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-09-25 19:00:02 +0200

    Initial commit.

---
+++ SpeakersList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeakersList
+//
+//  Created by Jorge D. Ortiz Fuentes on 25/9/15.
+//  Copyright © 2015 PoWWaU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
