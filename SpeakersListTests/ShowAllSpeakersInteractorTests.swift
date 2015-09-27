67f2aa4aad7b7026f6878c92ca324fc04902faaf
Jorge D. Ortiz-Fuentes <jdortiz@gmail.com>
2015-09-27 15:19:24 +0200

    Add unit tests for show all speakers interactor.

--- SpeakersListTests/ShowAllSpeakersInteractorTests.swift
+++ SpeakersListTests/ShowAllSpeakersInteractorTests.swift
@@ -13,11 +13,22 @@ import XCTest
 
 class ShowAllSpeakersInteractorTests: XCTestCase {
 
+    // MARK: - Parameters & Constants
+    
+    let speakerMainName = "Juanita Banana"
+    let speakerAltName = "Ellie Phant"
+    let speakerMainTitle = "Life and its consequences"
+    let speakerAltTitle = "My personal zoo"
+    let speakerMainSynopsis = "Lots of knowlege gathered through the years"
+    let speakerAltSynopsis = "I am not a zebra, neither are you."
+    let speakerMainDateSubmitted = NSDate(timeIntervalSince1970: 1000)
+    let speakerAltDateSubmitted = NSDate(timeIntervalSince1970: 2000)
+
     // MARK: - Test variables.
 
     var sut: ShowAllSpeakersInteractor!
-    var entityGateway: EntityGatewayProtocol!
-    var presenter: SpeakersListPresenterProtocol!
+    var entityGateway: EntityGatewayMock!
+    var presenter: SpeakersListPresenterMock!
     
 
     // MARK: - Set up and tear down.
@@ -50,18 +61,113 @@ class ShowAllSpeakersInteractorTests: XCTestCase {
     }
 
 
+    func testEntityGatewayIsSetFromInitializer() {
+        XCTAssertTrue((sut.entityGateway as! EntityGatewayMock) === entityGateway,
+            "Entity gateway must be set from the one provided in the initializer")
+    }
+
+
+    func testPresenterIsSetFromInitializer() {
+        XCTAssertTrue((sut.presenter as! SpeakersListPresenterMock) === presenter,
+            "Presenter must be set from the one provided in the initializer.")
+    }
+
+
+    func testSpeakersAreFetchedFromEntityGateway() {
+        sut.showAllSpeakers()
+        XCTAssertTrue(entityGateway.fetchAllSpeakersWasCalled,
+            "Speakers must be fetched from the entity gateway.")
+    }
+
+
+    func testRepoWithNoEntitiesGeneratesNoDisplayData() {
+        sut.showAllSpeakers()
+        XCTAssertEqual(presenter.displayData.count, 0,
+            "No data must be passed to the presenter when the repo is empty.")
+    }
+
+
+    func testEntityIstransformedIntoDisplayData() {
+        entityGateway.entities = [Speaker(name: speakerMainName, title: speakerMainTitle,
+            synopsis: speakerMainSynopsis, dateSubmitted: speakerMainDateSubmitted)]
+
+        sut.showAllSpeakers()
+
+        XCTAssertEqual(presenter.displayData.count, entityGateway.entities.count,
+            "Only one display data must be pased to the presenter per each entity of the repo.")
+        if presenter.displayData.count > 0 {
+            let speakerData = presenter.displayData[0]
+            XCTAssertEqual(speakerData.name, speakerMainName,
+                "Speaker name must be copied onto the display data.")
+            XCTAssertEqual(speakerData.title, speakerMainTitle,
+                "Speaker title must be copied onto the display data.")
+            XCTAssertEqual(speakerData.dateSubmitted, speakerMainDateSubmitted,
+                "Speaker submitedDate must be copied onto the display data.")
+        }
+    }
+
+
+    func testTwoEntitiesAreTransformedIntoDisplayData() {
+        entityGateway.entities = [Speaker(name: speakerMainName, title: speakerMainTitle,
+            synopsis: speakerMainSynopsis, dateSubmitted: speakerMainDateSubmitted),
+            Speaker(name: speakerAltName, title: speakerAltTitle,
+                synopsis: speakerAltSynopsis, dateSubmitted: speakerAltDateSubmitted)]
+
+        sut.showAllSpeakers()
+
+        XCTAssertEqual(presenter.displayData.count, entityGateway.entities.count,
+            "The number of display data structs must match the number of entities in the repo.")
+        if presenter.displayData.count > 0 {
+            let speakerData = presenter.displayData[0]
+            XCTAssertEqual(speakerData.name, speakerMainName,
+                "Speaker name must be copied onto the display data.")
+            XCTAssertEqual(speakerData.title, speakerMainTitle,
+                "Speaker title must be copied onto the display data.")
+            XCTAssertEqual(speakerData.dateSubmitted, speakerMainDateSubmitted,
+                "Speaker submitedDate must be copied onto the display data.")
+        }
+        if presenter.displayData.count > 1 {
+            let speakerData = presenter.displayData[1]
+            XCTAssertEqual(speakerData.name, speakerAltName,
+                "Speaker name must be copied onto the display data.")
+            XCTAssertEqual(speakerData.title, speakerAltTitle,
+                "Speaker title must be copied onto the display data.")
+            XCTAssertEqual(speakerData.dateSubmitted, speakerAltDateSubmitted,
+                "Speaker submitedDate must be copied onto the display data.")
+        }
+    }
+
+
     // MARK: - Stubs & Mocks
     
     class EntityGatewayMock: EntityGatewayProtocol {
 
+        // MARK: - Properties
+
+        var fetchAllSpeakersWasCalled = false
+        var entities: [Speaker] = []
+
+
+        // MARK: - Mocked Methods
+
         func fetchAllSpeakers() -> [Speaker] {
-            return []
+            fetchAllSpeakersWasCalled = true
+            return entities
         }
     }
 
 
     class SpeakersListPresenterMock: SpeakersListPresenterProtocol {
+
+        // MARK: - Properties
+
+        var displayData: [SpeakerDisplayData] = []
+
+
+        // MARK: - Mocked Methods
+        
         func presentAllSpeakers(speakers: [SpeakerDisplayData]) {
+            displayData = speakers
         }
     }
 }
